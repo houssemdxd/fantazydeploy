2a471100b7b4d6e981d8e0236ab1a60556da57fc
Houssem Khalfaoui <gameplayclip4@gmail.com>
2025-08-27 15:16:48 +0100

    add adress

--- nest-app/src/main.ts
+++ nest-app/src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
-  await app.listen(3000);
+await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
