719a065323a3be66f2436cfc83ab8e5ed3d213c3
Houssem Khalfaoui <gameplayclip4@gmail.com>
2025-08-30 19:04:54 +0100

    return flask-api

--- nest-app/src/apiservice/apiservice.service.ts
+++ nest-app/src/apiservice/apiservice.service.ts
@@ -99,7 +99,7 @@ async  getMatches(): Promise<any[]> {
   const matches: any[] = [];
 
   try {
-    const response = await fetch('http://127.0.0.1:5000matches');
+    const response = await fetch('http://flask-api:5000matches');
 
     if (!response.ok) {
       throw new Error(`API request failed with status: ${response.status}`);
@@ -122,7 +122,7 @@ async getMatchesByRoundSofa(roundId: number) {
     try {
       console.log("falsk1")
       // Replace with your Flask server URL and endpoint
-      const url = `http://127.0.0.1:5000/matches?round=${roundId}`;
+      const url = `http://flask-api:5000/matches?round=${roundId}`;
             console.log("falsk2")
 
       // Call the Flask API
@@ -153,7 +153,7 @@ async getMatchesByRoundSofa(roundId: number) {
 
 
   async getLineupsofa(matchId: string) {
-    const baseUrl = 'http://127.0.0.1:5000/lineup'; // Flask API URL
+    const baseUrl = 'http://flask-api:5000/lineup'; // Flask API URL
     const url = `${baseUrl}/${matchId}`;
 
     try {
@@ -173,7 +173,7 @@ async getMatchesByRoundSofa(roundId: number) {
 
   async getLiveUpdatefromsofa(matchId: string, homeCode: string, awayCode: string) {
     try {
-      const url = 'http://127.0.0.1:5000/events';
+      const url = 'http://flask-api:5000/events';
       const response = await firstValueFrom(
         this.httpService.get(url, {
           params: {
